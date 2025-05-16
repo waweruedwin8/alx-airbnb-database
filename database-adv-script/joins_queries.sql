b5a1013217129575c7c086773c321e066768fd65
Edwin Waweru <120776412+waweruedwin8@users.noreply.github.com>
2025-05-16 22:27:57 +0300

    Update joins_queries.sql, added codition to order by property name

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -27,6 +27,7 @@ SELECT
 FROM Property p
 LEFT JOIN "Review" r
     ON p.property_id = r.property_id;
+ORDER BY p.name;
 
 --Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
